08ed623202e15a9274105e77beebc27706faf16f
Diego Dallaqua <dldallaquapro@gmail.com>
2026-06-16 03:05:24 -0300

    h

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -37,9 +37,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "fb8d10aa5642bf4dbb53d997535a6509
 "icons/Icon-512.png": "8f974807faf6b4e0db9a09d0131a9ebf",
 "icons/Icon-maskable-192.png": "890fd277888a1cafb1bab26c2c6f1e96",
 "icons/Icon-maskable-512.png": "8f974807faf6b4e0db9a09d0131a9ebf",
-"index.html": "7394f6cd27ccfd9f8ef5006d7d8be463",
-"/": "7394f6cd27ccfd9f8ef5006d7d8be463",
-"main.dart.js": "65592e3ad1cecf0e53c690d660595ea1",
+"index.html": "e7e82bf5a1821b27d64b427cdfec0860",
+"/": "e7e82bf5a1821b27d64b427cdfec0860",
+"main.dart.js": "0fc3f9a74e024609239eda26d235dd7e",
 "manifest.json": "e342279d39b5b9233d4c8c0b756caeac",
 "version.json": "c0df2bc7e9ec869f21c7badf62b5d12b"};
 // The application shell files that are downloaded before a service worker can
